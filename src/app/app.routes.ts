a22d7aaa1ced1f58c0921cf4a34346eaf4af43c7
Sebastianm <sebaqd91@gmail.com>
2024-11-16 21:53:38 +0000

    add login and basic project components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { ProjectsComponent } from './pages/auth/projects/projects.component';
+import { TasksComponent } from './pages/tasks/tasks.component';
+import { RegisterComponent } from './pages/auth/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
+  { path: 'auth/login', component: LoginComponent },
+  { path: 'auth/register', component: RegisterComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'tasks', component: TasksComponent },
+];
